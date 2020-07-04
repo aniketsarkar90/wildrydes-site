4bf5e7621f252795a9af0c94a09d99edcd94c3d1
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-07-04 19:58:58 -0400

    Clean up
    removing aws resources ref

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 
 var _config = {
     cognito: {
-        userPoolId: 'us-east-1_Cx0ubLOyY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7m32cvloetri7uhc04q341s23e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1', // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://u9r26jqh06.execute-api.us-east-1.amazonaws.com/prod', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
+        invokeUrl: 'TEST', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
     }
 };
     
