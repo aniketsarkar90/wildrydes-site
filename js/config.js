a4d956a4fbfcd90b199e3d9a9841a9a319734f04
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-07-04 17:03:09 -0400

    added config
    added cognito user pool details

--- js/config.js
+++ js/config.js
@@ -1,10 +1,12 @@
-window._config = {
+
+var _config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Cx0ubLOyY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7m32cvloetri7uhc04q341s23e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1', // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'Base URL of your API including the stage', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
     }
 };
+    
